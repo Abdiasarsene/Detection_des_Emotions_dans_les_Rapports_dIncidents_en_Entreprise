17d96f6d8c202ed9bab8c16444203f0feff96c9c
Abdiasarsene <abdiasarsene@gmail.com>
2025-03-19 13:37:58 +0100

    Ajout de photo

--- Code_python/analysis.py
+++ Code_python/analysis.py
@@ -9,3 +9,14 @@ import seaborn as sns
 rapport_incident = pd.read_excel(r"D:\Projects\IT\Data Science & IA\Detection_Emotions_Rapports_Incidents\Text_data\incident_analysis.xlsx")
 
 # Nuage des mots
+
+
+
+
+from transformers import pipeline
+
+# Charger le modèle de sentiment en français
+sentiment_pipeline = pipeline("sentiment-analysis", model="cmarkea/distilcamembert-base-sentiment")
+
+# Appliquer sur tes textes
+rapport_incident ["Sentiment"] = rapport_incident ["Inutilite_supr"].apply(lambda x: sentiment_pipeline(x)[0]['label'])
